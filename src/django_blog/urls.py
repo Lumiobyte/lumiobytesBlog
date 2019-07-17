56210bc0ad13eb94d04022673d439f912443ebce
Lumiobyte <evanpartridge0@gmail.com>
2019-07-18 09:58:02 +1000

    Update urls.py make images load

--- src/django_blog/urls.py
+++ src/django_blog/urls.py
@@ -36,8 +36,7 @@ urlpatterns = [
     path('search/', search_view),
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
+from django.conf.urls.static import static
 
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
