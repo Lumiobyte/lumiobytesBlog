c51b161a1880fd635c0f3dee573bf11adff1b147
Lumiobyte <evanpartridge0@gmail.com>
2019-07-18 09:05:42 +1000

    Update settings.py for deployment

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1l(nm$+!f5$cvqtt157(gpd741$^kkyoyrjs6e43plf08fy#d7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["lumiobyte.com", "138.197.11.90"]
 
 
 # Application definition
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': 'bloguser',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -124,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'cdn')
+LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'static') # live cdn such as AWS S3
 STATICFILES_DIRS = [
